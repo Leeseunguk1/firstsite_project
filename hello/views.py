9919bcb44d70f0d8fbe7a907956e01d2373db60f
이승욱 <uk991221@naver.com>
2020-07-11 18:06:20 +0900

    add model

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Blog
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    blogs = Blog.objects
+    return render(request, 'home.html' , {'blogs' : blogs})
+
+def new(request):
+    return render(request, 'new.html')
+
+
+def create(request):
+       if(request.method == 'POST'):
+        post = Blog()
+        post.title = request.POST['title']
+        post.body = request.POST['body']
+        post.save()
+        return redirect('home')
